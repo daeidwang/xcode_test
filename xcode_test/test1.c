3e2bb1f4b5a831313440a361cd43333b45154d01
daeidwang <daeidwang@tencent.com>
2026-05-29 16:51:06 +0800

    Initial commit

---
+++ xcode_test/test1.c
@@ -0,0 +1,8 @@
+//
+//  test1.c
+//  xcode_test
+//
+//  Created by daeidwang on 2026/5/19.
+//
+
+#include "test1.h"
---
+++ xcode_test/test1.h
@@ -0,0 +1,13 @@
+//
+//  test1.h
+//  xcode_test
+//
+//  Created by daeidwang on 2026/5/19.
+//
+
+#ifndef test1_h
+#define test1_h
+
+#include <stdio.h>
+
+#endif /* test1_h */
